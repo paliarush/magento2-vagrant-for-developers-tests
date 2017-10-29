b13d84cee868b5eb40bb44ca8b4f613e78fc05ff
Alex Paliarush <apaliarush@magento.com>
2017-10-29 18:01:01 -0500

    Support for parallel tests execution

--- testsuite.sh
+++ testsuite.sh
@@ -1,5 +1,20 @@
 #! /usr/bin/env bash
 
-./testsuite-part1.sh
-./testsuite-part2.sh
-./testsuite-part3.sh
+pkill -f child-projects
+rm -rf ./tmp/child-projects/*
+
+testSuites=('testsuite-part1' 'testsuite-part2' 'testsuite-part3')
+for testSuiteName in ${testSuites[@]}; do
+    rsync -av --exclude 'tmp/child-projects/*' --exclude 'tmp/test/*' --exclude 'tmp/testsuite/*' . "./tmp/child-projects/${testSuiteName}" >/dev/null
+    logFile="./tmp/child-projects/${testSuiteName}.log"
+    touch "${logFile}"
+    time "./tmp/child-projects/${testSuiteName}/${testSuiteName}.sh" > "${logFile}" &
+done
+
+wait
+
+for testSuiteName in ${testSuites[@]}; do
+    cat "./tmp/child-projects/${testSuiteName}.log"
+done
+
+echo "Parallel Run Complete"
